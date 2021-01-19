33764fd496288861894b5c65f4ecd68d299153ed
Biswajit10 <biswajit1071@gmail.com>
2021-01-20 02:12:29 +0530

    Code Refactor

--- src/components/InputText.jsx
+++ src/components/InputText.jsx
@@ -1,27 +1,32 @@
 import React, { useState } from "react";
 
+const InputText = (props) => {
+  const [inputText, setInputText] = useState("");
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
 
-function InputText(props) {
-    const [inputText, setInputText] = useState("");
-    function handleChange(event) {
-        const newValue = event.target.value;
-        setInputText(newValue);
-    }
+  return (
+    <div className="form">
+      <form
+        onSubmit={(event) => {
+          inputText.trim() !== ""
+            ? props.dataItem(inputText)
+            : props.setErr(`Enter something in the input box to Add to the list
+(Note: Input cannot consist of only white spaces.)`);
 
-    return (
-        <div className="form">
-            <form onSubmit={(event) => {
-                props.dataItem(inputText);
-                setInputText("");
-                event.preventDefault();
-            }}>
-                <input onChange={handleChange} type="text" value={inputText} />
-                <button type="submit">
-                    <span>Add</span>
-                </button>
-            </form>
-        </div>
-    );
-}
+          setInputText("");
+          event.preventDefault();
+        }}
+      >
+        <input onChange={handleChange} type="text" value={inputText} />
+        <button type="submit" onBlur={() => props.setErr("")}>
+          <span>Add</span>
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default InputText;
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,27 +1,40 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
-function ListItem(props) {
+const ListItem = ({ id, data, deleteItem }) => {
   const [isDone, setDone] = useState(false);
 
-  function handleClick() {
-    setDone(preValue => {
+  const handleClick = (e) => {
+    setDone((preValue) => {
       return !preValue;
     });
-  }
+    e.preventDefault();
+  };
 
   return (
-    <div>
+    <div style={{ display: "flex" }}>
       <li
-        style={{ textDecoration: isDone ? "line-through" : "none" }}
+        style={{
+          textDecoration: isDone ? "line-through" : "none",
+          width: "90%",
+        }}
         onClick={handleClick}
-        onDoubleClick={() => {
-          props.onDoubleClick(props.id);
+      >
+        {data.inputText}
+      </li>
+      <svg
+        style={{ float: "right" }}
+        className="delete-icon"
+        focusable="false"
+        viewBox="0 0 24 24"
+        onClick={(e) => {
+          e.preventDefault();
+          deleteItem(id);
         }}
       >
-        {props.data}
-      </li>
+        <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" />
+      </svg>
     </div>
   );
-}
+};
 
 export default ListItem;
